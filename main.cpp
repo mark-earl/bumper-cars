1bac6ef860c0a3bf574e5cb03baccb5a963103dc
Mark Earl <mpe12@uakron.edu>
2023-10-22 12:38:34 -0400

    Fix random syntax

--- main.cpp
+++ main.cpp
@@ -1,78 +1,106 @@
-#define N_CARS 2 
-#define N_RIDERS 5 
-#define T_WANDER 100   /* each wandering time is between 0 to T_WANDER */ 
-#define T_BUMP 40      /* each bumping time is between 0 to T_BUMP */ 
-int COUNT_DOWN = 10;   /* Sim time: Total number of bumper car rides */
+#include <iostream>
+#include <time.h>
 
-int finish() { 
-if (COUNT_DOWN==0) return TRUE; else return FALSE; 
-} 
-/* Rider thread. rid is a number between 1 to N_RIDERS.  */ 
-void Rider(int rid) {   
-while (TRUE)  
-{ /* wander around for a random amount of time */ 
-Wander(rid, random()%T_WANDER); 
-GetInLine(rid); 
-TakeASeat(rid); 
-TakeARide(rid); 
-...    /* check for the condition to exit the while loop here */ 
-} 
-} 
-void GetInLine(int rid) {   
-... 
-printf("Rider %d gets in the waiting line.\n", rid); 
-} 
-void TakeASeat(int rid) { 
-...    
-} 
-void TakeARide(int rid) {   
-printf("Rider %d is taking the ride.\n", rid); 
-... 
-} 
-void Wander(int rid, int interval) { 
-printf("Rider %d is wandering around the park.\n", rid); 
-... 
-} 
-Page 3 
-3460:4/526 Lab 2: POSIX Threads and Semaphores 
-/* Bumper car thread. cid is a number between 1 to N_CARS. */ 
-void Car(int cid) { 
-while (TRUE) {  
-Load(cid); 
-Bump(cid, random()%T_BUMP); 
-Unload(cid); 
-...  /* decrease the COUNT_DOWN */ 
-...  /* check for the condition to exit the while loop */ 
-} 
-}     
-void Load(int cid) { 
-... 
-this_guy=WaitArea[LineHead]; 
-LineHead++; 
-printf("Car %d takes the rider %d.\n", cid, this_guy); 
-...  
-} 
-void Unload(int cid) { 
-... 
-printf("This ride of Car %d is over.\n"); 
-...  
-}  
-/* Displaying thread */ 
-void Display(int dummy) { 
-while (!finish()) 
-{ 
-printf("The current situation in the park is:\n"); 
-for (i=1 to N_CARS) 
-if (Car[i] is running) 
-print "Car i is running. The rider is ???" 
-else  
-print "Car i is not running." 
-for (i=1 to N_RIDERS) 
-if (Rider[i] is wandering) 
-print "Rider i is wandering" 
-else if (Rider[i] is waiting in line) 
-print "Rider i is waiting in line" 
-else 
-print "Rider i is in a car." 
-} 
-} 
+#define NUMBER_OF_CARS 2
+#define NUMBER_OF_RIDERS 5
+#define TIME_WANDER 100 /* each wandering time is between 0 to TIME_WANDER */
+#define TIME_BUMP 40    /* each bumping time is between 0 to TIME_BUMP */
+int COUNT_DOWN = 10;    /* Sim time: Total number of bumper car rides */
+
+int main()
+{
+    return 0;
+}
+
+int done()
+{
+    if (COUNT_DOWN == 0)
+        return true;
+    else
+        return false;
+}
+
+// Rider thread. rid is a number between 1 to NUMBER_OF_RIDERS.
+void Rider(int rid)
+{
+    while (true)
+    {
+        // wander around for a random amount of time
+
+        // Use current time as seed for random generator
+        srand(time(0));
+        Wander(rid, rand() % TIME_WANDER + 1); // + 1 because rand() ranges [0, N-1]
+        GetInLine(rid);
+        TakeASeat(rid);
+        TakeARide(rid);
+        // TODO: check for the condition to exit the while loop here
+    }
+}
+void GetInLine(int rid)
+{
+    // TODO
+    std::cout << "Rider " << rid << " gets in the waiting line.\n";
+}
+void TakeASeat(int rid)
+{
+    // TODO
+}
+void TakeARide(int rid)
+{
+    std::cout << "Rider " << rid << " is taking the ride.\n";
+    // TODO
+}
+void Wander(int rid, int interval)
+{
+    std::cout << "Rider " << rid << " is wandering around the park.\n";
+    // TODO
+}
+
+// Bumper car thread. cid is a number between 1 to NUMBER_OF_CARS.
+void Car(int cid)
+{
+    while (true)
+    {
+        Load(cid);
+        Bump(cid, random() % TIME_BUMP);
+        Unload(cid);
+        // TODO decrease the COUNT_DOWN
+        // TODO: check for the condition to exit the while loop
+    }
+}
+void Load(int cid)
+{
+    // TODO this_guy = WaitArea[LineHead];
+    LineHead++;
+    std::cout << "Car " << rid << " takes the rider " << this_guy << ".\n";
+    // TODO
+}
+
+void Unload(int cid)
+{
+    // TODO
+    std::cout << "This ride of Car " << cid << " is over.\n";
+    // TODO
+}
+
+// Displaying thread
+void Display(int dummy)
+{
+    while (!done())
+    {
+        std::cout << "The current situation in the park is:\n";
+        for (int i = 0; i < NUMBER_OF_CARS; ++i)
+            if (Car[i] is running)
+                std::cout << "Car i is running. The rider is ???";
+            else
+                std::cout << "Car i is not running.";
+
+        for (i = 1 to N_RIDERS)
+            if (Rider[i] is wandering)
+                std::cout << "Rider i is wandering";
+            else if (Rider[i] is waiting in line)
+                std::cout << "Rider i is waiting in line";
+            else
+                std::cout << "Rider i is in a car.";
+    }
+}
