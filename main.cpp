b15aa57570c7a5535eda50439d6e554d3e520892
Mark Earl <mpe12@uakron.edu>
2023-10-25 21:24:59 -0400

    Update makefile

--- main.cpp
+++ main.cpp
@@ -1,105 +1,78 @@
-// Libraries
 #include <iostream>
-#include <time.h>
+#include <thread>
+#include <vector>
+#include <semaphore.h>
+#include "Car.hpp"
+#include "Rider.hpp"
 
-// Constants
-#define NUMBER_OF_CARS 2   // Number of bumper cars
-#define NUMBER_OF_RIDERS 5 // Number of riders
-#define TIME_WANDER 100    // Each wandering time is between 0 to TIME_WANDER
-#define TIME_BUMP 40       // Each bumping time is between 0 to TIME_BUMP */
-int NUMBER_OF_RIDES = 10;  // Total number of bumper car rides (control length of execution)
+#define NUMBER_OF_CARS 2
+#define NUMBER_OF_RIDERS 5
+int NUMBER_OF_RIDES = 10;
 
-int main()
-{
-    // Use current time as seed for random generator
-    srand(time(0));
+// Waiting line to hold the riders
+std::vector<Rider> waitingRiders;
+
+sem_t waitingForRide;     // Semaphore to track empty slots in the buffer
+sem_t riding;  // Semaphore to track occupied slots in the buffer
+sem_t waitingLineMutex;    // Mutex to protect buffer access
+
+int main() {
+    // Initialize semaphores and other necessary resources
+    sem_init(&waitingForRide, 0, NUMBER_OF_RIDERS);
+    sem_init(&riding, 0, NUMBER_OF_CARS);
+    sem_init(&waitingLineMutex, 0, 1);
+
+    // Create car instances and rider instances
+    std::vector<Car> cars;
+    std::vector<Rider> riders;
+
+    // Set cid from [1, NUMBER_OF_CARS]
+    for (int i = 1; i <= NUMBER_OF_CARS; ++i) {
+        cars.push_back(Car(i));
+    }
+
+    // Set rid from [1, NUMBER_OF_RIDERS]
+    for (int i = 1; i <= NUMBER_OF_RIDERS; ++i) {
+        riders.push_back(Rider(i));
+    }
+
+    // Run the simulation for a specified number of rides
+    while (NUMBER_OF_RIDES > 0) {
+        // Logic for the simulation:
+        // 1. Allow cars to pick up riders when they're available.
+        // 2. Riders get in line, take a seat, and enjoy the ride.
+        // 3. Update ride counts and exit conditions.
+
+        // Reduce NUMBER_OF_RIDES when a ride is completed
+        // Check for your exit condition here
+
+        // For example, you might implement a simple exit condition as follows:
+        if (NUMBER_OF_RIDES == 0) {
+            break; // All rides are completed
+        }
+    }
+
+    // At this point, all rides are completed. Perform cleanup and join threads.
+
+    // Set done flags for all riders to signal them to leave the park.
+    for (Rider& rider : riders) {
+        rider.leavePark();
+    }
+
+    // Join all rider threads to ensure they finish before exiting.
+    for (Rider& rider : riders) {
+        pthread_join(rider.getRiderThread(), NULL);
+    }
+
+    // Join car threads (optional if your Car class uses pthreads).
+    for (Car& car : cars) {
+        pthread_join(car.getCarThread(), NULL);
+    }
+
+    // Destroy semaphores and release resources.
+    sem_destroy(&waitingForRide);
+    sem_destroy(&riding);
+    sem_destroy(&waitingLineMutex);
+
     return 0;
-}
-
-// Rider thread. rid is a number between 1 to NUMBER_OF_RIDERS.
-void Rider(int rid)
-{
-    while (true)
-    {
-        // wander around for a random amount of time
-        Wander(rid, rand() % TIME_WANDER + 1); // + 1 because rand() ranges [0, N-1]
-        GetInLine(rid);
-        TakeASeat(rid);
-        TakeARide(rid);
-        // TODO: check for the condition to exit the while loop here
-    }
-}
-void GetInLine(int rid)
-{
-    // TODO
-    std::cout << "Rider " << rid << " gets in the waiting line.\n";
-}
-void TakeASeat(int rid)
-{
-    // TODO
-    std::cout << "Rider " << rid << " takes a seat.\n";
-}
-void TakeARide(int rid)
-{
-    std::cout << "Rider " << rid << " is taking the ride.\n";
-    // TODO
-}
-void Wander(int rid, int interval)
-{
-    std::cout << "Rider " << rid << " is wandering around the park.\n";
-    // TODO
-}
-
-void Bump(int cid, int rand)
-{
-    // TODO
-}
-
-// Bumper car thread. cid is a number between 1 to NUMBER_OF_CARS.
-void Car(int cid)
-{
-    while (true)
-    {
-        Load(cid);
-        Bump(cid, rand() % TIME_BUMP + 1); // + 1 because rand() ranges [0, N-1]
-        Unload(cid);
-        // TODO decrease the NUMBER_OF_RIDES
-        // TODO: check for the condition to exit the while loop
-    }
-}
-void Load(int cid)
-{
-    // TODO this_guy = WaitArea[LineHead];
-    // TODO SHARED VARIABLE LineHead++;
-    std::cout << "Car " << cid << " takes the rider "; // TODO define: << this_guy << ".\n";
-    // TODO
-}
-
-void Unload(int cid)
-{
-    // TODO
-    std::cout << "This ride of Car " << cid << " is over.\n";
-    // TODO
-}
-
-// Displaying thread
-void Display(int dummy)
-{
-    while (NUMBER_OF_RIDES > 0)
-    {
-        std::cout << "The current situation in the park is:\n";
-        for (int i = 0; i < NUMBER_OF_CARS; ++i)
-            if (dummy) // TODO (Car[i] is running)
-                std::cout << "Car i is running. The rider is ???";
-            else
-                std::cout << "Car i is not running.";
-
-        for (int i = 0; i < NUMBER_OF_RIDERS; ++i)
-            if (dummy) // TODO (Rider[i] is wandering)
-                std::cout << "Rider i is wandering";
-            else if (dummy) // TODO (Rider[i] is waiting in line)
-                std::cout << "Rider i is waiting in line";
-            else
-                std::cout << "Rider i is in a car.";
-    }
 }
--- makefile
+++ makefile
@@ -1,1 +1,19 @@
-# TODO create make command
+CXX = g++
+CXXFLAGS = -std=c++11 -Wall -Wextra
+LDFLAGS = -lpthread -lstdc++
+
+SRCS = main.cpp Car.cpp Rider.cpp
+OBJS = $(SRCS:.cpp=.o)
+
+TARGET = bumper_cars.out
+
+all: $(TARGET) clean
+
+$(TARGET): $(OBJS)
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(LDFLAGS)
+
+%.o: %.cpp
+	$(CXX) $(CXXFLAGS) -c $<
+
+clean:
+	rm -f $(OBJS)
